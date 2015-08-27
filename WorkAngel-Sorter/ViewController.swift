b6355de52b957c89bea1da33f99617cc123aaa56
Vincenzo Favara <vin.favara@gmail.com>
2015-08-27 20:37:35 +0200

    prepare table

--- WorkAngel-Sorter/ViewController.swift
+++ WorkAngel-Sorter/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - DATASOURCE:
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        // TODO:
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
 
+    // MARK: - ACTIONS:
+    
+    @IBAction func sortTripAction(sender: AnyObject) {
+        sort()
+    }
+    
+    // MARK: - USER:
+    
+    func sort(){
+        
+    }
 
 }
 
