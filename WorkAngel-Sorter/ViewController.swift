132264e1bb0790cabe28cb01a0e817531a195a50
Vincenzo Favara <vin.favara@gmail.com>
2015-08-27 20:20:14 +0200

    Create project

---
+++ WorkAngel-Sorter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkAngel-Sorter
+//
+//  Created by Vincenzo Favara on 27/08/15.
+//  Copyright (c) 2015 Vincenzo Favara (VinSoft). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
